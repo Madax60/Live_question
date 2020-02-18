917132f63244ba66ddf870e6770a7711996c00c2
Mehdi <mehdisamni@hotmail.fr>
2020-02-18 11:37:49 +0100

    Ajout des fichier

---
+++ pages/question.php
@@ -0,0 +1,43 @@
+<?php
+    include_once ("../includes/top.php");
+?>
+
+<section class="container-fluid">
+    <article class="container">
+        <div class="container row">
+            <img src="../ressources/marque1.jpg" alt="logo de la personne qui a écrit la question" class="img-fluid col-2">
+            <p class="col-xs col-sm col-md col-lg">Mehdi | 2 like | <span class="font-italic">Jeux vidéo</span></p>
+        </div>
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Asperiores esse eum ipsa placeat praesentium totam ut vel voluptate?
+                Assumenda ex explicabo ipsum labore nam quidem voluptatum!
+                Assumenda nesciunt perferendis quam?
+            </p>
+        </div>
+    </article>
+</section>
+<section class="container-fluid">
+    <div class="container row">
+        <article class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <article class="container">
+                <div class="container row">
+                    <img src="../ressources/marque1.jpg" alt="logo de la personne qui a écrit la question" class="img-fluid col-2">
+                    <p class="col-xs-10 col-sm-10 col-md-10 col-lg-10">Mehdi | 2 like | <span class="font-italic">Jeux vidéo</span></p>
+                </div>
+                <div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Asperiores esse eum ipsa placeat praesentium totam ut vel voluptate?
+                        Assumenda ex explicabo ipsum labore nam quidem voluptatum!
+                        Assumenda nesciunt perferendis quam?
+                    </p>
+                </div>
+            </article>
+        </article>
+        <form action="question.php" class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+
+        </form>
+    </div>
+</section>

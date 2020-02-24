f85dd029d4d161cdbfddfb596774c13fd61e0274
Tony <tony.nguyen1999i@gmail.com>
2020-02-24 14:17:08 +0100

    problème centrage et responsive des logos, footer non finis, navbar probleme inversion en responsive

--- index.php
+++ index.php
@@ -2,7 +2,40 @@
 
 	require_once('./includes/header.php');
 ?>
-
+	<div class="container-fluid m-0 p-0 bg-dark" id="video">
+		<p class="text-center font-weight-bold pl-1" id="texte-video"><img src="images/iplay.png">"Nulla venenatis magna fringilla"</p>
+	</div>
+	<div class="container-fluid m-0 p-0 text-center">
+		<div class="text-center pt-5" id="partenaires">
+		<h3>Etiam laot, alique sceleris.</h3>
+		<div id="texte-partenaire" class="px-2 mx-2">
+			<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.</p>
+		</div>
+		<div id="logo-partenaires" class="pt-5 text-center col-xs-12 col-sm-6 col-md-8 col-lg-12">
+			<div class="">
+				<button type="button" class="couleur-marque ml-2 float-left"><img src="images/marque1.jpg" class="py-1 my-1" width="25px" height="30px"> <span class="px-1 mx-1">Kyan Boards</span></button>
+			</div>
+			<div class="">
+				<button type="button" class="couleur-marque ml-2  float-left"><img src="images/marque2.jpg" class="py-1 my-1" width="25px" height="30px"> <span class="px-1 mx-1">Atica</span></button>
+			</div>
+			<div class="">
+				<button type="button" class="couleur-marque ml-2 float-left"><img src="images/marque3.jpg" class="py-1 my-1" width="25px" height="30px"> <span class="px-1 mx-1">Treva</span></button>
+			</div>
+			<div class="">
+				<button type="button" class="couleur-marque ml-2 float-left"><img src="images/marque4.jpg" class="py-1 my-1" width="25px" height="30px"> <span class="px-1 mx-1">Kanba</span></button>
+			</div>
+			<div class="">
+				<button type="button" class="couleur-marque ml-2 float-left"><img src="images/marque5.jpg" class="py-1 my-1" width="25px" height="30px"> <span class="px-1 mx-1">Tvit Forms</span></button>
+			</div>
+			<div class="">
+				<button type="button" class="couleur-marque ml-2 float-left"><img src="images/marque6.jpg" class="py-1 my-1" width="25px" height="30px"> <span class="px-1 mx-1">Aven</span></button>
+			</div>
+			<div class="">
+				<button type="button" class="couleur-marque ml-2 float-left"><img src="images/marque7.jpg" class="py-1 my-1" width="25px" height="30px"> <span class="px-1 mx-1">Utosia</span></button>
+			</div>
+		</div>
+		</div>
+	</div>
 
 <?php
 	require_once('./includes/footer.php');
